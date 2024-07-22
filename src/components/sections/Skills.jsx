ff1abc9d4dc61a1d9f2dc9293c1d68934632d3c2
sanketpatel <patelsanketr98@gmail.com>
2024-07-22 11:03:32 -0400

    initial commit

---
+++ src/components/sections/Skills.jsx
@@ -0,0 +1,124 @@
+import {
+  BiLogoNetlify,
+  BiLogoPostgresql,
+  BiLogoTypescript,
+} from "react-icons/bi";
+import { CgVercel } from "react-icons/cg";
+import { DiGithubAlt, DiMongodb } from "react-icons/di";
+import { FaDocker } from "react-icons/fa";
+import {
+  FaAws,
+  FaCss3Alt,
+  FaGitAlt,
+  FaHtml5,
+  FaJava,
+  FaNodeJs,
+  FaReact
+} from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoFirebase } from "react-icons/io5";
+import {
+  SiAngular,
+  SiBootstrap,
+  SiExpress,
+  SiMysql,
+  SiNestjs,
+  SiPostman,
+  SiReact,
+  SiSpringboot,
+  SiTailwindcss
+} from "react-icons/si";
+
+function Skills() {
+  return (
+    <section id="skills" className="mx-4 lg:mx-20">
+      {/* =========== SKILLS TITLE =========== */}
+      <h4 className="text-4xl font-bold text-center mt-20 dark:text-white">
+        Technologies I Use.
+      </h4>
+      {/* =========== LIST OF SKILLS =========== */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 mt-8">
+        <span className="inline-flex items-center justify-between gap-x-2 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaHtml5 className="text-2xl" /> HTML
+        </span>
+
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaCss3Alt className="text-2xl" /> CSS
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <IoLogoJavascript className="text-2xl" /> JavaScript
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <BiLogoTypescript className="text-2xl" /> TypeScript
+        </span>
+
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaJava className="text-2xl" /> Java
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiSpringboot className="text-2xl" /> SpringBoot
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaReact className="text-2xl" /> React
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiExpress className="text-2xl" /> Express
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaNodeJs className="text-2xl" /> NodeJS
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiAngular className="text-2xl" /> Angular
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiReact className="text-2xl" /> React Native
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiBootstrap className="text-2xl" /> Bootstrap
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiTailwindcss className="text-2xl" /> Tailwind
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiNestjs className="text-2xl" /> NestJS
+        </span>
+        
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaGitAlt className="text-2xl" /> Git
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <DiGithubAlt className="text-2xl" /> Github
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <BiLogoNetlify className="text-2xl" /> Netlify
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <CgVercel className="text-2xl" /> Vercel
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <IoLogoFirebase className="text-2xl" /> Firebase
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiPostman className="text-2xl" /> Postman
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaAws className="text-2xl" /> AWS
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <DiMongodb className="text-2xl" /> MongoDB
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiMysql className="text-2xl" /> MySQL
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <BiLogoPostgresql className="text-2xl" /> PostgreSQL
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaDocker className="text-2xl" /> Docker
+        </span>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
---
+++ src/data/user_info.js
@@ -0,0 +1,161 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Sanket Patel",
+        description: "I'm a dedicated software engineer specializing in full-stack and mobile development. With a strong passion for solving complex problems, I continuously strive to enhance my skills and deliver high-quality, efficient solutions. I have extensive experience in a variety of programming languages, frameworks, and cloud technologies, and I thrive in dynamic, agile environments.",
+        role: "Full-Stack Developer",
+        photo: "../pp1.png",
+        email: "patelsanketr98@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+		github: "https://github.com/sanketpatel98",
+		linkedin: "https://www.linkedin.com/in/sanketpatel98/",
+		instagram: "https://www.instagram.com/_patel_sanket_/",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "Blogging Hub",
+			description: "Developed to learn full-stack web development using MongoDB, Express, React, and Node.js, providing blogging functionalities.",
+            technologies: "React, Express, Node, Mongo",
+			github: "https://github.com/sanketpatel98/MERN-Vlog.git",
+            link: "https://mern-blog-7ttj.onrender.com/"
+		},
+
+        {
+			title: "Harry Potter API",
+			description: "A simple Express.js API with CRUD operations, using AWS DynamoDB for managing and providing data on Harry Potter / Hogwarts characters.",
+			technologies: "Express, DynamoDB, AWS-SDK",
+			github: "https://github.com/sanketpatel98/AWS-Express-HarrypotterAPI.git",
+            link: ""
+		},
+
+        {
+			title: "NFT Marketplace",
+			description: "Developed a cross-platform NFT Marketplace app with React Native Expo CLI, using React Hooks and Figma for intuitive design.",
+			technologies: "Expo, React Native, Figma",
+			github: "https://github.com/sanketpatel98/react-native-nft-app.git",
+            link: ""
+		},
+
+		{
+			title: "Recipe App- Chef de Cuisine",
+			description: "Chef de Cuisine is a mobile app that simplifies cooking with pantry resources to discover recipes, manage ingredients and make it simple.",
+			technologies: "React Native, Express, GCP",
+			github: "https://github.com/sanketpatel98/capstoneProject.git",
+            link: ""
+		},
+
+		{
+			title: "Insurance Intelligence",
+			description: "Spring Boot app predicting flight delays using a Kaggle dataset and machine learning, with data analysis and visualization.",
+			technologies: "JAVA, Spring Boot, Machine Learning",
+			github: "https://github.com/sanketpatel98/SpringBoot.git",
+            link: ""
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Fanshawe College",
+            degree: "Post Graduation in Mobile Application Development",
+            duration: "2022 - 2023",
+            image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAnFBMVEX////0u7r99fX++vrztrXwoqDjKiHkOzXulJLv1tbyrKrgAADlRkHAWFrfrq7pamfkTkrmWFToZGHOgILJcnTDY2WsAADhsrLpdXKzFx3jAAC7RUj3y8nx4eK7AQmtAAr40tH53t3ypaP46+u7DxPBOz21DxX75ua5AADti4nMAADcFw3BAADqjIrhNjDlLibFIyPenJ2sHSTYKCM7FqAXAAAA1ElEQVR4Ac3QtQHCABQA0YsHPu7uEPf9Z0Oq6ABc+7rjj1NUAE3vRMPUAMvuoB59sWAgRgcOR4xlMpUZ88WoicvVms12N2O/WtZF0Q/H1Wo9PX3tvOSi6BW8itzu58duxvO1QBc5UMn5qTsDb2f1RS5QU3N3u8NX/W3QMJhswxu/IonVOmmxbJKdAXqKJ0FNVVPGZI9Q03eP51flXkHDMxitznfdNxfnJzcvotbPlF2+Yd6cAOvt86hLUMiN+Z52B5GrI6KQdaAhV3DEhi68AFxt/rcPFq8QFWFqZ0YAAAAASUVORK5CYII="
+        },
+        {
+            school: "Gujarat Technological University",
+            degree: "Bachelors in Computer Engineering",
+            duration: "2016 - 2020",
+            image: "https://upload.wikimedia.org/wikipedia/en/9/93/Gujarat-Technological-University-Logo.png"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Mobile Application Developer",
+            company: "Freelancing",
+            duration: "Jul 2023 - Dec 2023",
+            image: "https://seeklogo.com/images/F/freelancing-it-logo-9C5CC77D84-seeklogo.com.png",
+            descriptions: [
+                "Developed a React Native mobile application using Expo for iOS and Android.",
+                "Enhanced the inventory counting process by 80% through streamlined barcode scanning and live monitoring.",
+                "Ensured project cost efficiency by utilizing Google Sheets as the database with Google API and Google Apps Script.",
+                "Utilized Expo and React Native modules, including React Redux, for efficient state management.",
+                "Implemented Expo File System, Axios, and React Native Paper for enhanced functionality and a consistent UI/UX.",
+                "Successfully delivered a cost-effective & efficient mobile application for inventory counting that reduced 60% of physical work.",
+            ]
+        },
+        {
+            position: "Software Developer ",
+            company: "Softvan",
+            duration: "Jan 2021 - Dec 2021",
+            image: "https://i.ibb.co/Ns06KrS/1630569343300.jpg",
+            descriptions: [
+                "Transformed monolithic E-commerce web-application to microservices with Java (SpringBoot) backend for loosely coupled architecture.",
+                "Implemented Eureka for efficient microservices intercommunication, optimizing the architecture of the E-commerce platform",
+                "Developed dynamic and responsive E-commerce web applications by following effective Angular lifecycle hooks.",
+                "Executed Hibernate batch processing, successfully reducing transaction processing time by an impressive 30% for improved performance.",
+                "Integrated GitHub into a robust CI/CD pipeline, fostering a cohesive development workflow for Angular and Java (SpringBoot).",
+                "Managed agile workflows using Jira, ensuring streamlined project delivery and maintaining timely and smooth development cycles."
+            ]
+        },
+        {
+            position: "Junior Software Developer",
+            company: "KiwiMiles India",
+            duration: "Sept 2020 - Dec 2020",
+            image: "https://i.ibb.co/rfRpbZs/kiwimiles.jpg",
+            descriptions: [
+                "Engineered a seamless web application using Nest.js (Node.js) for the backend and Angular 8 for the front end.",
+                "Implemented Docker to encapsulate the application, that includes a Postgres database, facilitating ease of deployment and management.",
+                "Utilized Git source control to manage versioning and collaborated on development efforts efficiently.",
+                "Applied Agile methodologies, employing JIRA for sprint planning & tracking to ensure continuous enhancement of the application.",
+                "Developed a PDF quoting tool by nestjs-pdf, extracting & processing large datasets from a Postgres database based on user selections.",
+                "Optimized deployment with AWS Elastic Beanstalk and AWS EC2, enhancing the scalability and performance of the web app."
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Java Threads",
+            description: "Provided by LinkedIn Learning",
+            icon: "linkedinlearning",
+            link: "https://www.linkedin.com/learning/certificates/7d1c54b8ed3dd44ce8131dbdaac0f31f63fb8fdc0573672a5ab23dfeaff75144",
+        },
+        {
+            title: "Java Object-Oriented Programming",
+            description: "Provided by LinkedIn Learning",
+            icon: "linkedinlearning",
+            link: "https://www.linkedin.com/learning/certificates/3f943299deec3cb6c7cfed856e93e78aa83d4ae76a54a3aa1ff934958ded7431",
+        },
+        {
+            title: "Agile Software Development",
+            description: "Provided by LinkedIn Learning",
+            icon: "linkedinlearning",
+            link: "https://www.linkedin.com/learning/certificates/2c73f286ce23f947b6ceacf9ce98cab0d8a0147c2b7b6f20e874d1464f695296",
+        },
+        {
+            title: "Problem Solving",
+            description: "Provided by HackerRank",
+            icon: "hackerrank",
+            link: "https://www.hackerrank.com/certificates/34d8ec1bc9da",
+        },
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at patelsanketr98@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Sanket Patel. All Rights Reserved"
+}
+
+export default info
